ac97a1a4a3472da35e7e7a66b7fb86a2401bdd8f
Onnesok <ratulhasan9464@gmail.com>
2023-01-09 20:41:38 +0600

    java tumi ki bari jaba ?

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
-public class main{
-    public static void main(String[] args){
-        System.out.println("yo yo java tumi ki jaba ?");
+public class main {
+    public static void main(String[] args) {
+        System.out.println("yo yo java tumi ki bari jaba ?");
     }
 }
-//
