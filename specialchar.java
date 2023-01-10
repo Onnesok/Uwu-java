30efcf7394d538d611f44f2ddb90b521d68c4f7c
Onnesok <ratulhasan9464@gmail.com>
2023-01-10 11:14:34 +0600

    Special char and math syntax

--- specialchar.java
+++ specialchar.java
@@ -1,4 +1,25 @@
 public class specialchar {
-    
+    public static void main(String[] args) {
+        String txt = "Yo mama ki obostha ? \"Ratul\" kemon ase?\\";
+    System.out.println(txt);  // it's adding double quote on Ratul  and \\ adds \ in string
+    txt = "\nYo mama\nki obostha? Ratul \t kemon ap\bse? \fyo";
+    System.out.println(txt);
+
+    /*  Here
+    \n means new line
+    \t means tab
+    \b means backspace  ==>> look at ase banan
+    \f means form feed 
+    */
+
+    ////////////   java basic math  /////////////////////////// 
+
+    System.out.println(Math.max(50, 10));
+    System.out.println(Math.min(50, 10));
+    System.out.println(Math.random());  // random() returns a random value from 0(inclusive) to 1.0 (exclusive)
+    int random_num = (int)(Math.random() * 101); // this will randomize numbers from 0 to 100
+    System.out.println(random_num);
+
+    }
 }
-//
+//Special char and math syntax
